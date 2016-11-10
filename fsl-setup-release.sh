8f0bf4038c9d256afc3f846c11ce1bac60d6c81d
alex6235 <alex.cheng@advantech.com.tw>
2016-11-10 10:45:36 +0800

    Add meta-advantech and meta-openembedded meta-layer

--- fsl-setup-release.sh
+++ fsl-setup-release.sh
@@ -195,6 +195,8 @@ echo "BBLAYERS += \" \${BSPDIR}/sources/meta-openembedded/meta-python \"" >> $BU
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-openembedded/meta-filesystems \"" >> $BUILD_DIR/conf/bblayers.conf
 
 echo "BBLAYERS += \" \${BSPDIR}/sources/meta-qt5 \"" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \" \${BSPDIR}/sources/meta-advantech/meta-fsl-imx6 \"" >> $BUILD_DIR/conf/bblayers.conf
+echo "BBLAYERS += \" \${BSPDIR}/sources/meta-openembedded/meta-webserver \"" >> $BUILD_DIR/conf/bblayers.conf
 
 echo BSPDIR=$BSPDIR
 echo BUILD_DIR=$BUILD_DIR
